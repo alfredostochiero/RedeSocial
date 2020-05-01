d3ffabfac2cb133eb13b29be0e6e7c5d2e7d5509
Alfredo Stochiero <alfredo_stochiero@hotmail.com>
2020-05-01 13:04:11 -0300

    Search Controller and View created

--- src/routes.php
+++ src/routes.php
@@ -15,7 +15,7 @@ $router->post('/cadastro','LoginController@signupAction');
 $router->post('/post/new','PostController@new');
 
 
-$router->get('perfil/{id}/fotos','ProfileController@photos');
+$router->get('/perfil/{id}/fotos','ProfileController@photos');
 $router->get('/perfil/{id}/amigos','ProfileController@friends');
 $router->get('/perfil/{id}/follow','ProfileController@follow');
 $router->get('/perfil/{id}','ProfileController@index');      // primeiro a especifica, depois a rota mais geral
@@ -26,11 +26,14 @@ $router->get('/fotos','ProfileController@photos');
 
 
 $router->get('/sair','LoginController@logout');
+$router->get('/pesquisa','SearchController@index');
 
 
-//$router->get('/pesquisar');
 
-//$router->get('/sair');
-//$router->get('/amigos');
+
+
+
+
+
 //$router->get('/fotos');
 //$router->get('/config');
